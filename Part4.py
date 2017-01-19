9ce86dc81c51f5e11d8d4220df929aa3d925a116
sergg_grom <sergg.grom@gmail.com>
2017-01-19 22:15:11 +0300

    Chapter 4 evening continue with globals and  locals

--- Part4.py
+++ Part4.py
@@ -1,3 +1,4 @@
+"""
 disaster = True
 if disaster:
      print ("Woe!")
@@ -28,16 +29,17 @@ elif color == 'bee purple':
     print('I dont now what is it. but only bees can see it')
 else:
     print('Ive never heard of the color', color)
-
-
-x = 7
 """
+
+"""
+x = 7
+
 print(x==5)
 print(x==7)
 print(5<x)
 print(x<10)
 """
-
+"""
 #print(5<x and x>10)
 #print(5<x or x>10)
 #print(5<x and not x<10)
@@ -47,6 +49,7 @@ if some_list:
     print('There is something in here')
 else:
     print('Hey, it is empty')
+"""
 
 """"
 count = 1
@@ -296,15 +299,144 @@ def knigths(saying):
 print(knigths('Ni!'))
 """
 
-
+"""
 def knights2(saying):
     def inner2():
         return "We are the knights who say: '%s'" % saying
     return inner2
 
 
+a = knights2('Duck')
+b = knights2('Hasenpfeffer')
+print(type(a))
+print(type(b))
 
-                   
-     
+print(a)
+print(b)
+print(a())
+print(b())
+"""
+
+"""
+def edit_story(words, func):
+     for word in words:
+          print(func(word))
+
+
+stairs = ['thud', 'meow', 'thud', 'hiss']
+
+
+def enliven(word):
+     return word.capitalize() + '!'
+
+edit_story(stairs, enliven)
+
+edit_story(stairs, lambda word: word.capitalize() + '!')
+"""
+
+"""
+print(sum(range(1, 101)))
+"""
+
+"""
+def my_range(first=0, last=10, step=1):
+     number = first
+     while number < last:
+          yield number
+          number += step
+
+print(my_range)
+ranger = my_range(1,5)
+print(ranger)
+
+
+for x in ranger:
+     print (x)
+"""
+
+
+"""
+def square_it(func):
+     def new_function(*args, **kwargs):
+          result = func (*args,**kwargs)
+          return result*result
+     return new_function
+
+def document_it(func):
+     def new_function(*args, **kwargs):
+          print('Running function:', func.__name__)
+          print('Positional arguments:', args)
+          print('Keyword arguments:',kwargs)
+          result = func(*args, **kwargs)
+          print('Result:', result)
+          return result
+     return new_function
+
+
+
+@document_it
+@square_it
+def add_ints(a, b):
+     return a + b
+"""
+
+
+
+
+
+"""
+print(add_ints(3,5))
+"""
+
+"""
+cooler_add_ints = document_it(add_ints)
+cooler_add_ints(3,5)
+"""
+
+animal = 'fruitbat'
+
+"""
+def print_global():
+     print('inside print_global:', animal)
+
+print('at the top level:', animal)
+print_global()
+"""
+"""
+def change_and_print_global():
+     print('inside chage_and_print_global:', animal)
+     global animal
+     animal =  'wombat'
+     print('after the change:', animal)
+change_and_print_global()
+
+def change_local():
+     animal = 'wombat'
+     print('inside change_local:', animal, id(animal))
+     print('locals:', locals())
+
+change_local()
+
+print(id(animal))
+
+print('globals:', globals())
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
