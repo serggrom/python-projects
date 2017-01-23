79c6936a25e2a30443d312e46e7ae21ea9d103e2
sergg_grom <sergg.grom@gmail.com>
2017-01-23 17:53:04 +0300

    cont Chapter6

--- Part6.py
+++ Part6.py
@@ -308,7 +308,29 @@ print(second == third)
 print(first)
 """
 
+"""
+class Bill():
+    def __init__(self, description):
+        self.description = description
 
+class Tail():
+    def __init__(self,length):
+        self.length = length
+
+
+class Duck():
+    def __init__(self, bill, tail):
+        self.bill = bill
+        self.tail = tail
+    def about(self):
+        print('This duck has a', bill.description, 'bill and a', tail.length, 'tail')
+
+
+tail = Tail('long')
+bill = Bill('wide orange')
+duck = Duck(bill, tail)
+duck.about()
+"""
 
 
 
