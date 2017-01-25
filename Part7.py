e27de497dedf878fff83bcda88a77e626afea980
sergg_grom <sergg.grom@gmail.com>
2017-01-25 18:12:43 +0300

    cont Chapter 7

--- Part7.py
+++ Part7.py
@@ -73,7 +73,7 @@ place4 = place_byte.decode('windows-1252')
 print(place4)
 """
 
-
+"""
 actor = 'Richard Gere'
 cat = 'Chester'
 weight = 28
@@ -82,7 +82,7 @@ print("My wife's favorite actor is %s" % actor)
 print("Our cat %s weights %s pounds" % (cat, weight))
 
 n = 42
-f = 4.03
+f = 7.03
 s = 'string cheese'
 
 print('%d %f %s' % (n, f, s))
@@ -92,16 +92,63 @@ print('%10d %10f %10s' % (n, f, s))
 print('%-10d %-10f %-10s' % (n, f, s))
 print('%10.4d %10.4f %10.4s' % (n, f, s))
 print('%.4d %.4f %.4s' % (n, f, s))
-print()
+print('%*.*d %*.*f %*.*s' % (10, 4, n, 10, 4, f, 10, 4, s))
+print('{} {} {}'.format(n, f, s))
+print('{2} {0} {1}'.format(f, s, n))
+print('{n} {f} {s}'.format(n=42, f=7.03, s='string cheese'))
+
+d = {'n': 42, 'f': 7.03, 's': 'string cheese'}
+
+print('{0[n]} {0[f]} {0[s]} {1}'.format(d, 'other'))
+
+print('{0:d} {1:f} {2:s}'.format(n, f, s))
+
+print('{n:d} {f:f} {s:s}'.format(n=42, f=7.03, s='string cheese'))
+
+print('{0:10d} {1:10f} {2:10s}'.format(n,f,s))
+
+print('{0:>10d} {1:>10f} {2:>10s}'.format(n, f, s))
+
+print('{0:<10d} {1:<10f} {2:<10s}'.format(n, f, s))
+
+print('{0:^10d} {1:^10f} {2:^10s}'.format(n, f, s))
 
 
+print('{0:>10d} {1:>10.4f} {2:>10.4s}'.format(n, f, s))
+
+print('{0:!^20s}'.format('BIG SALE'))
+"""
 
 
+import re
+
+result = re.match('You', 'Young Frankenstein')
+
+youPattern = re.compile('You')
+
+result2 = youPattern.match('Young Frankenstein')
 
 
+source = 'Young Frankenstein'
+m = re.match('You', source)
+if m:
+    print(m.group())
 
+m = re.match('^You', source)
+if m:
+    print((m.group()))
 
+m = re.match('Frank', source)
+if m:
+    print(m.group())
 
+m = re.search('Frank', source)
+if m:
+    print(m.group())
+
+m = re.match('.*Frank', source)
+if m:
+    print(m.group())
 
 
 
