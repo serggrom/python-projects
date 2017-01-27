f1b19a5512367b172069b4b5e900118cddfe0dc6
sergg_grom <sergg.grom@gmail.com>
2017-01-27 17:57:04 +0300

    end Chapter 7

--- Part7.py
+++ Part7.py
@@ -259,7 +259,7 @@ the_byte_array = bytearray(range(0, 256))
 print(the_bytes)
 """
 
-
+"""
 import struct
 
 valid_png_header = b'\x89PNG\r\n\x1a\n'
@@ -282,22 +282,120 @@ print(0x8d)
 print(struct.pack('>L', 154))
 print(struct.pack('>L', 141))
 
+print(struct.unpack('>2L', data[16:24]))
+print(struct.unpack('>16x2L6x', data))
+"""
+
+"""
+import binascii
+valid_png_header = b'\x89PNG\r\n\x1a\n'
+print(binascii.hexlify(valid_png_header))
+print(binascii.unhexlify(binascii.hexlify(valid_png_header)))
+"""
+
+import unicodedata
+
+mystery = '\U0001f4a9'
+
+print(mystery)
+print(unicodedata.name(mystery))
+
+pop_bytes = mystery.encode('utf-8')
+print(pop_bytes)
+
+pop_string = pop_bytes.decode('utf')
+print(pop_string)
+
+roast = 'roast beef'
+ham = 'ham'
+head = 'head'
+clam = 'clam'
+
+print('My kitty cat likes %s\n'
+      'My kitty cat likes %s\n'
+      'My kitty cat fell on %s\n'
+      'And now thinks he is %s' % (roast, ham, head, clam))
 
 
 
+letter = '''Dear {salutation} {name},
+Thank you for your letter. We are sorry that our {product} {verbed} in your
+{room}. Please note that it should never be used in a {room}, especially
+near any {animals}.
+Send us your receipt and {amount} for shipping and handling. We will send
+you another {product} that, in our tests, is {percent}% less likely to
+have {verbed}.
+Thank you for your support.
+Sincerely,
+{spokesman}
+{job_title}'''
 
 
+response = {
+    'salutation': 'customer',
+    'name': 'John',
+    'product': 'phone',
+    'verbed': 'placed',
+    'room': 'home',
+    'animals': 'cats',
+    'amount': 'one hundred',
+    'percent': '95',
+    'spokesman': 'retail man',
+    'job_title': 'CEO'
+}
+
+print(letter.format(**response))
 
 
+import re
+mammoth = '''
+We have seen thee, queen of cheese,
+Lying quietly at your ease,
+Gently fanned by evening breeze,
+Thy fair form no flies dare seize.
+All gaily dressed soon you'll go
+To the great Provincial show,
+To be admired by many a beau
+In the city of Toronto.
+Cows numerous as a swarm of bees,
+Or as the leaves upon the trees,
+It did require to make thee please,
+And stand unrivalled, queen of cheese.
+May you not receive a scar as
+We have heard that Mr. Harris
+Intends to send you off as far as
+The great world's show at Paris.
+Of the youth beware of these,
+For some of them might rudely squeeze
+And bite your cheek, then songs or glees
+We could not sing, oh! queen of cheese.
+We'rt thou suspended from balloon,
+You'd cast a shade even at noon,
+Folks would think it was the moon
+About to fall and crush them soon.
+'''
+
+print(re.findall(r'\bc\w*', mammoth))
+
+print(re.findall(r'\bc\w{3}\b', mammoth))
+
+print(re.findall(r'\b\w*r\b', mammoth))
+
+print(re.findall(r'\b\w*[aieou]{3}[^aieou\s]*\w*\b', mammoth))
+
+import binascii
+import struct
+gif = bytes(binascii.unhexlify(b'47494638396101000100800000000000ffffff21f9') +
+            binascii.unhexlify(b'0401000000002c000000000100010000020144003b'))
+print(gif)
 
 
-
-
-
-
-
-
-
+if gif[:6] == b'GIF89a':
+    print('True gif')
+    width, height = struct.unpack('<2H', gif[6:10])
+    print(width, height)
+    print(gif[6:8])
+    print(gif[8:10])
 
 
 
